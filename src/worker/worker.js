78ca7cc51699d825e30f44cd702a5ab9b30c4e48
diazwebapp <diazwebapp@gmail.com>
2025-05-28 22:36:30 -0400

    hecha pagina de perfiles de pago

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -1,13 +1,58 @@
 // worker.js
+let bcv = 0, paralelo = 0
+const fetchRates = async () => {
+  try {
+    const req = await fetch("https://pydolarve.org/api/v2/dollar?page=alcambio&format_date=default&rounded_price=true", {
+      headers: {
+        'Accept': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+        Authorization: 'Bearer JhZZj99yQrGoTOGdpNpOvQ'
+      }
+    })
+    if (!req.ok) return false
+    const res = await req.json()
+    bcv = res.monitors.bcv.price
+    paralelo = res.monitors.enparalelovzla.price
+    // Envía a todos los puertos conectados
+    broadcast({
+      response: {
+        rates: { bcv, paralelo }
+      }
+    });
+  } catch (error) {
+    console.error("reintentando en 2s... " + error)
+    setTimeout(fetchRates, 2000)
+  }
+}
 const ports = new Set();
-
-self.onconnect = (connectEvent) => {
+function broadcast(message) {
+  ports.forEach(port => {
+    port.postMessage(message);
+  });
+}
+self.onconnect = async (connectEvent) => {
   const port = connectEvent.ports[0];
   ports.add(port);
-  
-  port.onmessage = (event) => {
-    
-    // Puedes responder al cliente
-    port.postMessage({ response: event.data });
+
+  port.onmessage = async (event) => {
+      // Puedes responder al cliente
+      if (event.data.setTasa) {
+        broadcast({
+          response: {
+            currentTasa: event.data.setTasa
+          }
+        })
+      }
+
+      if (event.data === 'getRates') {
+        // Si el cliente solicita tasas explícitamente
+        if (bcv == 0 || paralelo == 0) {
+          await fetchRates()
+        }
+      }
+
   };
+  
+    
 };
+fetchRates()
